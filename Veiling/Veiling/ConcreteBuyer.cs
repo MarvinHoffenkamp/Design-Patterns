3046289142eddfae3a862a1d60de3083f3148010
Kariemdv <56026990+Kariemdv@users.noreply.github.com>
2022-03-27 13:02:43 +0200

    Added buyer interface and class

---
+++ Veiling/Veiling/ConcreteBuyer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Veiling.ObjectsOfSale;
+
+namespace Veiling
+{
+    class ConcreteBuyer : Buyer
+    {
+        private int buyersNumber;
+        private double wallet;
+        private List<ObjectOfSale> boughtObjects;
+
+        public ConcreteBuyer()
+        {
+
+        }
+
+        public void bit(double price)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
