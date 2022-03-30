4bc3f43a6c6bef49a56a5f298594a76b4580f3ac
Kariemdv <56026990+Kariemdv@users.noreply.github.com>
2022-03-30 16:54:28 +0200

    Removed unnecessary code

--- Veiling/Veiling/Program.cs
+++ Veiling/Veiling/Program.cs
@@ -10,23 +10,6 @@ namespace Veiling
     {
         static void Main(string[] args)
         {
-
-            /*
-             * Testing for Factory Method
-             *
-            int[] fordMeasurments = new int[3] {200, 150, 140};
-            Car ford = new Car("Ford", fordMeasurments, 5000.00);
-            Console.WriteLine("Car: "+ ford.turnOn());
-
-            int[] yamahaMeasurments = new int[3] { 200, 150, 140 };
-            Ship yamaha = new Ship("Yamaha", yamahaMeasurments, 25000.00);
-            Console.WriteLine("Ship: " + yamaha.turnOn());
-
-            int[] suzukiMeasurments = new int[3] { 200, 150, 140 };
-            Motorcycle suzuki = new Motorcycle("Suzuki", suzukiMeasurments, 2500.00);
-            Console.WriteLine("Motorcycle: " + suzuki.turnOn());
-            */
-
             Simulator.start();
         }
     }
